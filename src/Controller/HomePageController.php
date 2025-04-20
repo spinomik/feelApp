60411cbd48c66028c44048a12ed68f218f434033
Mikołaj Majewski <spinomik@spinomik.local>
2025-04-20 10:27:13 +0200

    Added about me, edited homePage

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -11,8 +11,12 @@ final class HomePageController extends AbstractController
     #[Route('/', name: 'app_home_page')]
     public function index(): Response
     {
-        return $this->render('home_page/index.html.twig', [
-            'controller_name' => 'HomePageController',
-        ]);
+        return $this->render('home_page/index.html.twig', []);
+    }
+
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('about/index.html.twig', []);
     }
 }
